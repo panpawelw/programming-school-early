386c7669666abe7d3b7502bf6cca889af690663f
pjm77 <probably.just.me77@gmail.com>
2018-03-14 14:12:24 +0100

    basic demo menu added

---
+++ src/programming_school/Demo.java
@@ -0,0 +1,14 @@
+package programming_school;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("\n PROGRAMMING SCHOOL "
+				+ "\n Java MySQL Project \n"
+				+ "\n Select option"
+				+ "\n 1. TESTING"
+				+ "\n 2. ADMIN INTERFACE"
+				+ "\n 3. USER INTERFACE \n"
+				+ "\n > ");
+	}
+}
